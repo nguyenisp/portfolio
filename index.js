dab544434343fa4d15c47d305fd8d59f24d32b92
nguyenne <nguyenne.200507@gmail.com>
2021-06-24 20:19:25 +0700

    Add the button to play random music

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-// @ts-check
 const playMusic = () => {
-  var audio = document.getElementById("audio");
+  const num = Math.floor(Math.random() * 3) + 1;
+  var audio = document.getElementById(`audio${num}`);
   audio.play();
-}
+  document.getElementById('button-play').style.display = 'none'
+};
