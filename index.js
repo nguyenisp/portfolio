bf60736d8a16ba16f78dab1093c65786bd9fa875
nguyenne <nguyentuannguyen200507@gmail.com>
2021-07-02 19:52:42 +0700

    Remove the music feature and unnecessary files

--- index.js
+++
@@ -1,6 +0,0 @@
-const playMusic = () => {
-  const num = Math.floor(Math.random() * 3) + 1;
-  var audio = document.getElementById(`audio${num}`);
-  audio.play();
-  document.getElementById('button-play').style.display = 'none'
-};
